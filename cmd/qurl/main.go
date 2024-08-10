f956553ff53ed31dd2748600197457ed50132f5d
Brian Baugher <bdb7hq@virginia.edu>
2024-08-10 16:13:49 -0400

    initial commit

---
+++ cmd/qurl/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/brian-baugher/qurl/internal/print"
+
+func main() {
+	print.Print("hello world from pkg")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brian-baugher/qurl
+
+go 1.22.2
